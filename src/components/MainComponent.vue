7ea2973c74db0eef62be7f23de2b37c2eceec965
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2022-12-20 18:40:06 +0100

    Fixed Bootstrap markup in MainComponent.vue

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,19 +1,29 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
   <div class="container" v-if="role === 'ADMIN'">
-    <br>
-    <div class="col-12">
-      <RouterLink to="/manage-doctors" type="button" class="btn btn-light">Gérer les dossiers des médecins</RouterLink>
-    </div>
-    <br>
-    <div class="col-12">
-      <RouterLink to="/manage-patient-files" type="button" class="btn btn-light">Gérer les dossiers patients
-      </RouterLink>
+    <div class="row justify-content-center g-5 py-3 mb-3">
+      <div class="col-md-6 col-lg-4">
+        <div class="card p-0 text-center">
+          <img src="../assets/img/doctor.jpg" class="card-img-top" alt="médecin">
+          <div class="card-body">
+            <RouterLink to="/manage-doctors" type="button" class="btn btn-outline-primary stretched-link">Gérer les dossiers des
+              médecins</RouterLink>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4">
+        <div class="card p-0 text-center">
+          <img src="../assets/img/patient.jpg" class="card-img-top" alt="patient">
+          <div class="card-body">
+            <RouterLink to="/manage-patient-files" type="button" class="btn btn-outline-primary stretched-link">Gérer les dossiers patients
+            </RouterLink>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
   <ManageFiles type="patientFile" v-else-if="role === 'DOCTOR'" />
-  <PatientFile v-else-if="role === 'PATIENT'"/>
-  <div class="container" v-else>Type inconnu</div>
+  <PatientFile v-else-if="role === 'PATIENT'" />
 </template>
 
 <!-- eslint-disable prettier/prettier -->
@@ -25,7 +35,7 @@ import PatientFile from "./PatientFile.vue";
 
 export default {
   name: "MainComponent",
-  components : {
+  components: {
     ManageFiles, PatientFile,
   },
   computed: {
@@ -36,4 +46,5 @@ export default {
 
 <!-- eslint-disable prettier/prettier -->
 <style>
+
 </style>
