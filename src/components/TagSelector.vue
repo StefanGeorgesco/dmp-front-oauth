14bec2cdf929a8824b546a555908bf5c7cb50953
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2022-12-20 17:32:50 +0100

    Fixed Bootstrap markup in TagSelector.vue

--- src/components/TagSelector.vue
+++ src/components/TagSelector.vue
@@ -1,16 +1,31 @@
 <!-- eslint-disable prettier/prettier -->
 <template>
-    <div class="tag-input" style="margin: 0;">
-        <div class="tag" v-for="o in selectedOptions" :key="o.id">
-            {{ o.id }} - {{ o.description }}
-            <span @click="remove(o)"></span>
-        </div>
-        <input @keyup.esc="clear" @blur="delayedClear" ref="input" v-model="searchString" type="text">
-        <div class="options-list" v-show="filteredOptions.length > 0">
-            <div class="tag-option" v-for="o in filteredOptions" :key="o.id" @click="add(o)">
+    <div class="d-flex align-items-center form-control p-0 m-0" :class="{ 'error': hasError, 'noError': noError }">
+        <div class="position-relative d-flex flex-wrap flex-fill m-0 p-0 ps-1">
+            <div class="btn btn-sm btn-primary d-inline-flex align-items-center py-1 px-1 my-1 me-1" v-for="o in selectedOptions" :key="o.id">
                 {{ o.id }} - {{ o.description }}
+                <span class="btn btn-close btn-close-white ms-1" @click="remove(o)"></span>
+            </div>
+            <input @keyup.esc="clear" @blur="delayedClear" ref="input" v-model="searchString" type="text"
+                class="flex-fill py-1 m-1" style="outline: none; border: none;" placeholder="Ajouter...">
+            <div class="position-absolute top-100 start-0 mt-2 p-2 border rounded shadow bg-white overflow-auto"
+                style="z-index: 1000; max-height: 50vh;" v-show="filteredOptions.length > 0">
+                <div class="tag-option" v-for="o in filteredOptions" :key="o.id" @click="add(o)">
+                    {{ o.id }} - {{ o.description }}
+                </div>
             </div>
         </div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#dc3545"
+            class="bi bi-exclamation-circle mx-2" v-show="hasError" viewBox="0 0 16 16">
+            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+            <path
+                d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="#198754" class="bi bi-check flex-shrink-0"
+            v-show="noError" viewBox="0 0 16 16">
+            <path
+                d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+        </svg>
     </div>
 </template>
 
@@ -23,7 +38,15 @@ export default {
         options: {
             type: Array,
             required: true,
-        }
+        },
+        hasError: {
+            type: Boolean,
+            default: false,
+        },
+        noError: {
+            type: Boolean,
+            default: false,
+        },
     },
     data() {
         return {
@@ -62,62 +85,15 @@ export default {
 
 <!-- eslint-disable prettier/prettier -->
 <style scoped>
-.tag-input {
-    position: relative;
-    border: 1px solid #ced4da;
-    border-radius: 0.375rem;
-    display: flex;
-    flex-wrap: wrap;
-    padding: 0;
-    margin: 0;
+.error {
+    border-color: #dc3545;
 }
 
-.tag-input>.tag {
-    flex: 0 1 auto;
-    border-radius: 0.375rem;
-    background-color: #0d6efd;
-    padding: 0.25em 1em;
-    margin: 0.25em;
-    color: white;
+.noError {
+    border-color: #198754;
 }
 
-.tag-input>.tag>span:after {
-    display: inline-block;
-    content: "\00d7";
-    margin: 0 0 0 1em;
-    font-size: large;
-    font-weight: bold;
-}
-
-.tag-input>.tag>span:hover {
-    cursor: pointer;
-}
-
-.tag-input>input {
-    border: none;
-    outline: none;
-    flex: 1 0 auto;
-    margin: 0.25em;
-    padding: 0.25em;
-}
-
-.tag-input>.options-list {
-    position: absolute;
-    top: 100%;
-    left: 0;
-    right: 0;
-    z-index: 99;
-    background-color: #ffffff;
-    border: 1px solid #ced4da;
-    box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.2);
-    padding: 0.25em 0 0.5em 0;
-}
-
-.options-list .tag-option {
-    padding-left: 1em;
-}
-
-.options-list .tag-option:hover {
+.tag-option:hover {
     cursor: pointer;
     background-color: #eeeeee;
 }
