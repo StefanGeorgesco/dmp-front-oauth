78f0330cbff917a70e746b36b2e28b69ca984934
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2022-12-22 15:55:20 +0100

    Change button message in FileCard.vue

--- src/components/FileCard.vue
+++ src/components/FileCard.vue
@@ -70,13 +70,8 @@
         </div>
         <div class="card-footer" style="min-height: 3.5rem;">
             <RouterLink v-if="type === 'patientFile' && role === 'DOCTOR' && canEditKnown && canEdit" :to="viewFileUrl"
-                class="btn btn-primary d-inline-flex align-items-center px-3" role="button">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                    class="bi bi-pencil me-2" viewBox="0 0 16 16">
-                    <path
-                        d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-                </svg>
-                Editer
+                class="btn btn-primary px-3" role="button">
+                Accéder au dossier
             </RouterLink>
             <button v-if="role === 'ADMIN' && !updatingReferringDoctor" type="button"
                 class="btn btn-danger d-inline-flex align-items-center px-3" data-bs-toggle="modal"
