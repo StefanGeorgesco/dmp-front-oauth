27e08a94c916c06f9fd2045bdb591d620c18fdb6
StefanGeorgesco <stefan.georgesco@yahoo.fr>
2023-03-10 16:34:17 +0100

    Update setting for dev container.

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,12 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      "^/api": {
-        target: "http://localhost:8080",
-        ws: true,
-        changeOrigin: true,
-      },
-    },
+    // allow outer access in container
+    host: '0.0.0.0',
+    watch: {
+        usePolling: true
+    }
+    //
   },
 
   plugins: [vue()],
